8c220de19b5717edb38fbb6263256606468162d8
tokko <andreg@kth.se>
2015-11-12 23:03:02 +0100

    unit test for getindexofmin

---
+++ TravelingSalesmanSolver/SolverTests/SolverTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using TravelingSalesmanSolver;
+
+namespace SolverTests
+{
+    [TestFixture]
+    public class SolverTests
+    {
+        [Test]
+        public void TestGetPath()
+        {
+
+        }
+
+        [Test]
+        public void TestGetIndexOfMin()
+        {
+            var list = new List<double> {3, 6, 1, 5, 3, 9, 19};
+
+            var min = list.IndexOfMin();
+
+            Assert.That(min, Is.EqualTo(2));
+        }
+    }
+}
