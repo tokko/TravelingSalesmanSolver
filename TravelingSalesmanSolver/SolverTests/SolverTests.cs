de043fc9766f0793d0b1215a684e77e41c75e0a7
tokko <andreg@kth.se>
2015-11-13 08:17:37 +0100

    2opt does not work

--- TravelingSalesmanSolver/SolverTests/SolverTests.cs
+++ TravelingSalesmanSolver/SolverTests/SolverTests.cs
@@ -10,12 +10,6 @@ namespace SolverTests
     public class SolverTests
     {
         [Test]
-        public void TestGetPath()
-        {
-
-        }
-
-        [Test]
         public void TestGetIndexOfMin()
         {
             var list = new List<double> {3, 6, 1, 5, 3, 9, 19};
@@ -39,7 +33,7 @@ namespace SolverTests
         }
 
         [Test]
-        public void TestGetpath()
+        public void TestGetPath()
         {
             var nodes = new Node[10];
             for (var i = 0; i < nodes.Length; i++)
@@ -50,6 +44,72 @@ namespace SolverTests
                 nodes[i - 1].Next = node;
                 node.Previous = nodes[i - 1];
             }
+
+            var res = nodes.GetPath().ToList();
+            Assert.That(res[0], Is.EqualTo(0));
+            for (var i = 1; i < res.Count; i++)
+            {
+                Assert.That(res[i], Is.EqualTo(i));
+            }
+        }
+
+        [Test]
+        public void Test2Opt()
+        {
+            var nodes = new Node[4];
+            for (var i = 0; i < nodes.Length; i++)
+            {
+                var node = new Node(i);
+                nodes[i] = node;
+                if (i <= 0) continue;
+                nodes[i - 1].Next = node;
+                node.Previous = nodes[i - 1];
+            }
+
+            var distances = new double[nodes.Length][];
+            distances[0] = new[]{0, 1, 0, 0.5};
+            distances[1] = new[]{1, 0, 0.5, 1};
+            distances[2] = new[]{0, 0.5, 0, 1};
+            distances[3] = new[]{0.5, 1, 1, 0};
+
+            Solver.TwoOpt(nodes[0], nodes[3], distances);
+
+            Assert.That(nodes[0].Next.Index, Is.EqualTo(2));   
+            Assert.That(nodes[1].Next.Index, Is.EqualTo(3));   
+            Assert.That(nodes[2].Next, Is.EqualTo(1));   
+            Assert.That(nodes[3].Next.Index, Is.Null);
+               
+            Assert.That(nodes[0].Previous, Is.Null);   
+            Assert.That(nodes[1].Previous.Index, Is.EqualTo(3));   
+            Assert.That(nodes[2].Previous.Index, Is.EqualTo(1));   
+            Assert.That(nodes[3].Previous.Index, Is.EqualTo(0));   
+        }
+
+        [Test]
+        [Ignore]
+        public void TestGetPathOpted()
+        {
+            var nodes = new Node[10];
+            for (var i = 0; i < nodes.Length; i++)
+            {
+                var node = new Node(i);
+                nodes[i] = node;
+                if (i <= 0) continue;
+                nodes[i - 1].Next = node;
+                node.Previous = nodes[i - 1];
+            }
+
+            var distances = new double[nodes.Length][];
+            distances[2] = new double[nodes.Length];
+            distances[5] = new double[nodes.Length];
+            distances[3] = new double[nodes.Length];
+            distances[6] = new double[nodes.Length];
+            distances[2][3] = 1;
+            distances[3][2] = 1;
+            distances[5][6] = 1;
+            distances[6][5] = 1;
+
+            Solver.TwoOpt(nodes[2], nodes[5], distances);
 
             var res = nodes.GetPath().ToList();
             Assert.That(res[0], Is.EqualTo(0));
--- TravelingSalesmanSolver/TravelingSalesmanSolver/Solver.cs
+++ TravelingSalesmanSolver/TravelingSalesmanSolver/Solver.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace TravelingSalesmanSolver
 {
+    [DebuggerDisplay("{Index}")]
     public class Node
     {
         public int Index { get; set; }
